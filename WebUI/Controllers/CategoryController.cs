1d24b93a7e855c32930d4ef8beab2b646efcc925
Kurtuluş Öcal <78833568+kurtulusocL@users.noreply.github.com>
2025-04-09 02:13:09 +0300

    Update CategoryController.cs

--- WebUI/Controllers/CategoryController.cs
+++ WebUI/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebUI.Controllers
 {
     [ExceptionHandler]
+    [Authorize(Roles = "Admins")]
     public class CategoryController : Controller
     {
         readonly ICategoryService _categoryService;
