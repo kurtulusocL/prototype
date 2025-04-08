f5172ab56eb7bf7e44f8fc896a97d0332900da72
Kurtuluş Öcal <78833568+kurtulusocL@users.noreply.github.com>
2025-04-09 02:13:55 +0300

    Update StockController.cs

--- WebUI/Controllers/StockController.cs
+++ WebUI/Controllers/StockController.cs
@@ -7,6 +7,7 @@ namespace WebUI.Controllers
 {
     [AuditLog]
     [ExceptionHandler]
+    [Authorize(Roles = "Admins")]
     public class StockController : Controller
     {
         readonly IUnitInStockService _unitInStockService;
