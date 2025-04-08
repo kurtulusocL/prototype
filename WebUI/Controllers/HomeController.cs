c1b13ae9a3522fa2b7adb5f5556033d3a315d595
Kurtuluş Öcal <78833568+kurtulusocL@users.noreply.github.com>
2025-04-09 02:12:22 +0300

    Update HomeController.cs

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace WebUI.Controllers
 {
     [AuditLog]
     [ExceptionHandler]
+    [Authorize(Roles = "Admins")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
