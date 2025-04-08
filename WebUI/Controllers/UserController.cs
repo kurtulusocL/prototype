f59035d9d86adeec8fc14aa5f9005389b1add3e5
Kurtuluş Öcal <78833568+kurtulusocL@users.noreply.github.com>
2025-04-09 02:14:11 +0300

    Update UserController.cs

--- WebUI/Controllers/UserController.cs
+++ WebUI/Controllers/UserController.cs
@@ -7,6 +7,7 @@ namespace WebUI.Controllers
 {
     [AuditLog]
     [ExceptionHandler]
+    [Authorize(Roles = "Admins")]
     public class UserController : Controller
     {
         readonly IUserService _userService;
